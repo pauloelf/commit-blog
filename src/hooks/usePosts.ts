7ef7cea1c300bd0c6cc72f9e79ec7402ca6d6033
Paulo Sergio <contato.paulofws@gmail.com>
2025-09-05 21:07:55 -0300

    feat: created usePost hook to fetch posts

---
+++ src/hooks/usePosts.ts
@@ -0,0 +1,27 @@
+import { useQuery } from "@tanstack/react-query"
+import api from "@/lib/axios"
+
+const fetchPosts = async (category?: string, currentPage?: number) => {
+  const response = await api.get(`/posts`, {
+    params: {
+      populate: "images",
+      sort: "publishedAt:desc",
+      pagination: { page: currentPage, pageSize: 6 },
+      filters:
+        category && category !== "todos"
+          ? { category: { slug: { $eq: category } } }
+          : {},
+    },
+  })
+  return response.data
+}
+
+export function usePosts(category?: string, currentPage?: number) {
+  return useQuery({
+    queryKey: ["posts", category, currentPage],
+    queryFn: () => fetchPosts(category, currentPage),
+    staleTime: 1000 * 60 * 5,
+    retry: 1,
+    refetchOnWindowFocus: false,
+  })
+}
