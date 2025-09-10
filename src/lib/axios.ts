f78dc46eb676b798d0a1da1abe0eb77947c332e4
Paulo Sergio <contato.paulofws@gmail.com>
2025-09-10 20:19:20 -0300

    refactor: using .env

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: "https://motivated-advice-17027cdcc0.strapiapp.com/api",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
