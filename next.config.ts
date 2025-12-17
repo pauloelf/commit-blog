b42915bce90aa90cfdc12f18ab7d7d051df8fc6e
Paulo Sergio <contato.paulofws@gmail.com>
2025-12-17 13:26:41 -0300

    fix: update image hostname to i.imgur.com in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "motivated-advice-17027cdcc0.media.strapiapp.com",
+        hostname: "i.imgur.com",
         port: "",
         pathname: "/**",
       },
